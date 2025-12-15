ad09dab55cbc760c97ec991056bad006b849d73e
mdryraf-ux <mdryraf@gmail.com>
2025-12-15 19:35:24 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    WEBHOOK_URL: "https://discord.com/api/webhooks/1450192996036382801/pdG7a6tCfNmdT0RZMzSUYqZXxj9oXbSXwiuTyJAT86HX5k6OyvRtzGZNp7HUxysa7F_n",
+    REDIRECT_URL: "https://shop.playoxide.com/" 
+};
